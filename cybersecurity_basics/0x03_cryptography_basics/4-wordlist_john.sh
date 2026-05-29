e10ddfe2e179351f91450fdca1e806363115869c
Rahima <13765@holbertonstudent.com>
2026-05-29 10:01:35 +0000

    add 4

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=rockyou.txt "$1" > /dev/null 2>&1 && john --show "$1" | cut -d: -f2 > 4-password.txt
+sh -c "john --format=raw-sha256 --wordlist=rockyou.txt '$1' > /dev/null && john --show --format=raw-sha256 '$1' | cut -d: -f2 > 4-password.txt"
