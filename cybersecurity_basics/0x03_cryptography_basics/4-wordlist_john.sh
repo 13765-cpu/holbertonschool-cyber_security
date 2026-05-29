dddc6027a0b891e573f66d924029f99998df14d4
Rahima <13765@holbertonstudent.com>
2026-05-29 09:56:39 +0000

    add 4

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-john --wordlist=rockyou.txt "$1" > /dev/null 2>&1
-john --show "$1" | cut -d: -f2 > 4-password.txt
+/usr/sbin/john --wordlist=rockyou.txt "$1" > /dev/null 2>&1
+/usr/sbin/john --show "$1" | cut -d: -f2 > 4-password.txt
