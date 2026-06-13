d3e414b9e15b9927a6af2c8e5c2c0075d6ca5101
13765-cpu <13765@holbertonstudents.com>
2026-06-13 14:24:56 -0400

    add cryptography basics scripts

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh -c "john --format=raw-md5 --wordlist=rockyou.txt '$1' > /dev/null && john --show --format=raw-md5 '$1' | cut -d: -f2 > 4-password.txt"
+john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
